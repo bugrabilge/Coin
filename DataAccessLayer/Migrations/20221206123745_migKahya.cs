3836c2050b2906da2f091ead1f22879ee7aa827f
Furkan Eralp Kahyaoğlu <furkan.kahyaoglu.3@gmail.com>
2022-12-06 16:25:13 +0300

    Login page updated.

---
+++ DataAccessLayer/Migrations/20221206123745_migKahya.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class migKahya : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
